d953bdb9d8cc8596813a54f39fc695a8924d0dd0
Spencer Rich <136946994+Kryze7773@users.noreply.github.com>
2024-11-26 18:06:55 -0700

    Update script.js
    initial scripting

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+//tbh idrk what this shit does but oh well more to learn
 
+// Script to display an alert when the page loads
+window.addEventListener('load', () => {
+	alert('Welcome to my GitHub Page!');
+});
+
+// Function to change the text of the paragraph when clicked
+document.addEventListener('DOMContentLoaded', () => {
+	const paragraph = document.querySelector('p');
+
+	paragraph.addEventListener('click', () => {
+		paragraph.textContent = 'You just clicked the paragraph!';
+	});
+});
