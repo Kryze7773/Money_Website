92cace0eda8c8d26b0027cacf59b9ebe6e445602
Spencer Rich <136946994+Kryze7773@users.noreply.github.com>
2024-11-26 18:23:24 -0700

    Update script.js
    no notification

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //tbh idrk what this shit does but oh well more to learn
 
 // Script to display an alert when the page loads
-window.addEventListener('load', () => {
-	alert('Welcome to my GitHub Page!');
-});
+//window.addEventListener('load', () => {
+	//alert('Welcome to my GitHub Page!');
+//});
 
 // Function to change the text of the paragraph when clicked
 document.addEventListener('DOMContentLoaded', () => {
